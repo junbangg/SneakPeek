2fe74f866f0be5768f0b48abfadc863c6a51ff1f
junbangg <wnstjr4620@naver.com>
2020-11-19 19:41:52 +0900

    Initial Commit

---
+++ SneakPeek/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SneakPeek
+//
+//  Created by Jun suk Bang on 2020/11/19.
+//  Copyright © 2020 Jun suk Bang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
